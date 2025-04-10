903b77add6c38a11a30ed0688b49178530742bcb
QuangDat <ngodat25112005@gmail.com>
2025-04-10 23:50:41 +0700

    fix repository layer

--- spring-non-jwt/src/main/java/com/javaweb/repository/impl/BuildingRepositoryImpl.java
+++ spring-non-jwt/src/main/java/com/javaweb/repository/impl/BuildingRepositoryImpl.java
@@ -18,7 +18,7 @@ import com.javaweb.util.StringUtils;
 
 @Primary
 @Repository
-public class BuildingRespositoryImpl implements BuildingRepository{
+public class BuildingRepositoryImpl implements BuildingRepository{
 	@PersistenceContext
 	private EntityManager entityManager;
 	private void buildJoinClause(BuildingSearchBuilder buildingSearchBuilder, StringBuilder join) {
@@ -88,8 +88,8 @@ public class BuildingRespositoryImpl implements BuildingRepository{
 			where.append(typeCode.stream().map(code -> "'" + code + "'").collect(Collectors.joining(", ")));
 			where.append(")");
 		}
-		Long rentPriceFrom = buildingSearchBuilder.getRentAreaFrom();
-		Long rentPriceTo = buildingSearchBuilder.getRentAreaTo();
+		Long rentPriceFrom = buildingSearchBuilder.getRentPriceFrom();
+		Long rentPriceTo = buildingSearchBuilder.getRentPriceTo();
 		if (rentPriceFrom != null) {
 			where.append(" AND b.rentPrice >= " + rentPriceFrom);
 		}
